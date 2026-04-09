a3f38f17883592536e8676c769951b7dd1bf764d
RayeYYY <190476203@qq.com>
2026-04-09 18:33:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  distDir: 'out',
-  basePath: '/vercel-fanmail',
-  assetPrefix: '/vercel-fanmail/'
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
