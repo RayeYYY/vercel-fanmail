13323d72220a6a47359c212ef4262ebcaf96a02d
RayeYYY <190476203@qq.com>
2026-04-09 18:27:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // 必须加这个，变成纯静态
-  distDir: 'out',
+  output: 'export', // 关键：把项目打成纯静态
+  distDir: 'out',   // 静态文件输出到out文件夹
+  basePath: '/vercel-fanmail', // 关键：适配GitHub Pages的子路径
+  assetPrefix: '/vercel-fanmail/', // 关键：静态资源路径适配
 }
 
 module.exports = nextConfig
