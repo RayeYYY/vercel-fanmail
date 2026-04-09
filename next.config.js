1900052d56cf1e4e31153368ea421d197356ae8d
RayeYYY <190476203@qq.com>
2026-04-09 18:06:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 删掉 output: 'export'，让Netlify正常部署Next.js服务端路由
+  trailingSlash: true,
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
